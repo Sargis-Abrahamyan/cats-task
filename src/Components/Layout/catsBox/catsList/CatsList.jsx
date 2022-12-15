158c3dacd48adc3548da2d7087625a36fdd605d9
Sargis-Abrahamyan <sargis.abrahamyan.2023@gmail.com>
2022-12-15 12:53:21 +0400

    update catsList

--- src/Components/Layout/catsBox/catsList/CatsList.jsx
+++ src/Components/Layout/catsBox/catsList/CatsList.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
+
 import styles from "./scss/catsList.module.scss";
 
 const CatsList = ({ cats }) => {
+    const [fullScreenImg, setFullScreenImg] = useState("")
+
     return (
         <div className={styles.catsBlock}>
-            <img src={cats.url} className={styles.catsList} alt={"cat"} />
+            <img src={cats.url} className={styles.catsList} alt={"cat"} onClick={() => setFullScreenImg(cats.url)} />
+
+            {fullScreenImg && (
+                <div className={styles.fullScreen}>
+                    <div className={styles.fullScreenBlock}>
+                        <img src={fullScreenImg} alt={"cat"} />
+                        <button onClick={() => setFullScreenImg("")}>X</button>
+                    </div>
+
+                </div>
+            )}
         </div>
     )
 };
